aa2723f7ec38c39d1564804feadfb15ec39dfec5
tomoyan1 <133628017+tomoyan1@users.noreply.github.com>
2026-05-05 11:00:22 +0900

    基本機能

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,15 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!
   def index
     @tasks = current_user.tasks
   end
 
   def new
-    @task = Task.new
+    @task = Task.new(task_type: params[:task_type])
+  end
+
+  def show
+    @task = current_user.tasks.find(params[:id])
   end
 
   def create
@@ -16,9 +21,27 @@ class TasksController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+  def edit
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def update
+    @task = current_user.tasks.find(params[:id])
+    if @task.update(task_params)
+      redirect_to root_path, notice: 'タスクが更新されました。'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    task = current_user.tasks.find(params[:id])
+    task.destroy
+    redirect_to root_path, notice: 'タスクが削除されました。'
+  end
 
   private
   def task_params
-    params.require(:task).permit(:name, :description, :due_date, :task_address, :task_type)
+    params.require(:task).permit(:name, :description, :due_date, :task_address, :task_type, :image)
   end
 end
