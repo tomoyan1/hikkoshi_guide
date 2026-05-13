24b5287d39d12dcea36d264ec33ebbcbc64ef382
tomoyan1 <133628017+tomoyan1@users.noreply.github.com>
2026-05-13 16:03:19 +0900

    レイアウトの修正

--- app/javascript/copy.js
+++ app/javascript/copy.js
@@ -1,12 +1,21 @@
-window.addEventListener('turbo:load',function(){
-  const copyButtons = document.querySelectorAll('.address-copy-button')
-  
-  copyButtons.forEach(function(button){
-    button.addEventListener('click',function(){
-    const address = button.getAttribute("data-address")
-    navigator.clipboard.writeText(address)
-   
+window.addEventListener('turbo:load', function() {
+  const copyButtons = document.querySelectorAll('.address-copy-button');
+
+  copyButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      const address = button.getAttribute("data-address");
+
+      navigator.clipboard.writeText(address).then(() => {
+        const originalText = button.innerHTML;
+
+        button.innerHTML = 'コピーしました！';
+        button.classList.add('copied-state'); 
+
+        setTimeout(() => {
+          button.innerHTML = originalText;
+          button.classList.remove('copied-state');
+        }, 2000);
+      });
     });
   });
 });
-
