4be3c3c39e30df5488d42796d7d407cae6127bda
tomoyan1 <133628017+tomoyan1@users.noreply.github.com>
2026-05-14 12:50:39 +0900

    タスク一斉追加の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -8,4 +9,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :old_zipcode, :old_address, :new_zipcode, :new_address, :move_out_date, :move_in_date])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :old_zipcode, :old_address, :new_zipcode, :new_address, :move_out_date, :move_in_date])
   end
+
+  private
+
+   def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
+    end
+   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -5,7 +5,8 @@ class Category < ActiveHash::Base
     { id: 3, name: '通信・ネット', class_name: 'category-net' },
     { id: 4, name: '各種契約・変更', class_name: 'category-contract' },
     { id: 5, name: '住まい・梱包', class_name: 'category-living' },
-    { id: 6, name: '買い物・その他', class_name: 'category-other' } 
+    { id: 6, name: '買い物・その他', class_name: 'category-other' },
+    { id: 7, name: '新生活・買い物リスト', class_name: 'category-newlife' }
   ]
   include ActiveHash::Associations
   has_many :tasks
